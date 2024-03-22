5347f03745e2605bb706f2e904d5748cf5644705
VI-RaYaN609 <158507680+VI-RaYaN609@users.noreply.github.com>
2024-03-22 01:27:20 +0100

    Update JAVA.js

--- INIT-WEB/JAVA.js
+++ INIT-WEB/JAVA.js
@@ -1,28 +1,29 @@
-function result (){
-    const select = document.querySelectorAll("select")
-    const inputs = document.querySelectorAll("input")
-    const price1= 100*select[0].value
-    const price2= 100*select[1].value
-    const price3= 100*select[2].value
-    const sum  = price1*inputs[0].value + price2*inputs[1].value + price3*inputs[2].value
-    let exoneration = 0
-    let state = true
-    if(inputs[0].value<0 ||inputs[0].value>9999){state = false}
-    if(inputs[1].value<0 ||inputs[1].value>9999){state = false}
-    if(inputs[2].value<0 ||inputs[2].value>9999){state = false}
-    if (!inputs[3].checked){
-        exoneration = sum*0.19
+function preview (){
+    const list = document.querySelectorAll("select")
+    const Quantity = document.querySelectororAll("input")
+    var condition = true
+    if(Quantity[0].value<0 ||Quantity[0].value>1000){condition = false}
+    if(Quantity[1].value<0 ||Quantity[1].value>1000){condition = false}
+    if(Quantity[2].value<0 ||Quantity[2].value>1000){condition = false}
+    if (!Quantity[3].checked){
+        exoneration = prixtotal*0.19
     }
+    var exoneration = 0
+    const prixproduit1= 100*list[0].value
+    const prixproduit2= 100*list[1].value
+    const prixproduit3= 100*list[2].value
+    const prixtotal  = prixproduit1*Quantity[0].value + prixproduit2*Quantity[1].value + prixproduit3*Quantity[2].value
     let reduction = 0
-    if(inputs[5].checked){reduction = 0.05}
-    if(inputs[6].checked){reduction = 0.1}
-    if(state){
-    document.write(`<!DOCTYPE html>
+    if(Quantity[5].checked){reduction = 0.05}
+    if(Quantity[6].checked){reduction = 0.1}
+    if(condition==true){
+    document.write(`
+    <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>orders</title>
         <style>      
          table{
             margin-top: 20px;
@@ -30,19 +31,19 @@ function result (){
             th{
                 font-size: 25px;
             }
-            @media (max-width: 1200px) {
+            @media (max-width: 1120px) {
                 th{
                     font-size: 20px;
                     transition: 0.5s ease-out;
                 }
             }
-            @media (max-width : 900px){
+            @media (max-width : 850px){
                 th{
                     font-size: 17.5px;
                     transition: 0.5s ease-out;
                 }
             }
-            @media (max-width: 450px) {
+            @media (max-width: 550px) {
                 th{
                     font-size: 15px;
                     transition: 0.5s ease-out;
@@ -60,22 +61,22 @@ function result (){
                 <th class="strong">Amount</th>
             </tr>
             <tr>
-                <th>Product ${select[0].value}</th>
-                <th>${price1} DA</th>
-                <th>${inputs[0].value}</th>
-                <th>${price1*inputs[0].value} DA</th>
+                <th>Product ${list[0].value}</th>
+                <th>${prixproduit1} DA</th>
+                <th>${Quantity[0].value}</th>
+                <th>${prixproduit1*Quantity[0].value} DA</th>
             </tr>
             <tr>
-                <th>Product ${select[1].value}</th>
-                <th>${price2} DA</th>
-                <th>${inputs[1].value}</th>
-                <th>${price2*inputs[1].value} DA</th>
+                <th>Product ${list[1].value}</th>
+                <th>${prixproduit2} DA</th>
+                <th>${Quantity[1].value}</th>
+                <th>${prixproduit2*Quantity[1].value} DA</th>
             </tr>
             <tr>
-                <th>Product ${select[2].value}</th>
-                <th>${price3} DA</th>
-                <th>${inputs[2].value}</th>
-                <th>${price3*inputs[2].value} DA</th>
+                <th>Product ${list[2].value}</th>
+                <th>${prixproduit3} DA</th>
+                <th>${Quantity[2].value}</th>
+                <th>${prixproduit3*Quantity[2].value} DA</th>
             </tr>
             <tr>
                 <th colspan="3">Exoneration Amount(19%)</th>
@@ -83,22 +84,23 @@ function result (){
             </tr>
             <tr>
                 <th colspan="3">Reduction Amount</th>
-                <th>${reduction*100}% (${sum*reduction}DA)</th>
+                <th>${reduction*100}% (${prixtotal*reduction}DA)</th>
             </tr>
             <tr>
                 <th colspan="2"></th>
                 <th>Net Amount</th>
-                <th>${sum} DA</th>
+                <th>${prixtotal} DA</th>
             </tr>
             <tr>
                 <th colspan="2"></th>
                 <th>Brut Amount</th>
-                <th>${sum+exoneration-reduction*sum} DA</th>
+                <th>${prixtotal+exoneration-reduction*prixtotal} DA</th>
             </tr>
         </table>
         <a class="hyperlinks" id="back" href="order.html">Other Order Entry</a>
         <script src="../index.js"></script>
     </body>
-    </html>`)
+    </html>
+    `)
 }
 }
