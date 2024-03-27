fdb7491c3a06a4d7184fbece6e73891cf871194d
VI-RaYaN609 <158507680+VI-RaYaN609@users.noreply.github.com>
2024-03-27 23:59:24 +0100

    test.cpp

--- C++/Library Manager/test.cpp
+++ C++/Library Manager/test.cpp
@@ -7,20 +7,18 @@ using namespace std;
 int main(){
     ifstream file("books.txt");
     if( file.is_open() ){
-        string line,title,author,genre;
-        int pos,price,ISBN;
+        string line,information[5];
+        int pos;
         while ( getline(file , line) ){
             int i  = 0;
             while(i < 5){
                 pos = line.find(";");
                 cout << line << "  " << pos << endl;
-                title = line.substr(0,pos);
+                information[i] = line.substr(0,pos);
                 line = line.substr(pos+1);
                 i++;
             }
-
         }
     }
-
     return 0;
 }
